4305a0206c6346b64d84d503221655a1b4493989
julitus <julioqq29@gmail.com>
2018-07-06 02:38:51 -0500

    Rusell score

---
+++ rusell.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import unicodedata
+
+DICTONARY_FILE_ = "DICT_v1.csv"
+F_D_ = {'w': 1, 'v_m': 2, 'v_o': 3, 'a_m': 5, 'a_o': 6}
+SEPARATOR_ = ","
+
+def removeAccents(s):
+	nfkd_form = unicodedata.normalize('NFKD', s.decode('unicode-escape'))
+	return str(u"".join([c for c in nfkd_form if not unicodedata.combining(c)]))
+
+def loadDict():
+	dicty = dict()
+	i = 0
+	infile = open(DICTONARY_FILE_, 'r')
+	infile.readline().strip('\n')
+	for line in infile:
+		row = line.strip('\n').split(SEPARATOR_)
+		if (row[F_D_['v_o']] != 'NA'):
+			s = removeAccents(row[F_D_['w']])
+			dicty[s] = {'v': [float(row[F_D_['v_m']]), float(row[F_D_['v_o']])], 'a': [float(row[F_D_['a_m']]), float(row[F_D_['a_o']])]}
+			i += 1
+	print `i` + " words loaded"
+	return dicty
+
+def getSentimentScore(tokens, dictByTopic, dictTokens):
+	num_v, den_v, num_a, den_a = 0, 0, 0, 0
+	for i in range(0, len(tokens)):
+		td = dictByTopic[tokens[i]] if type(dictByTopic) is dict else dictByTopic
+		num_v += ((td * dictTokens[tokens[i]]['v'][0]) / dictTokens[tokens[i]]['v'][1])
+		den_v += (td / dictTokens[tokens[i]]['v'][1])
+		num_a += ((td * dictTokens[tokens[i]]['a'][0]) / dictTokens[tokens[i]]['a'][1])
+		den_a += (td / dictTokens[tokens[i]]['a'][1])
+	return num_v / den_v, num_a / den_a
+
+d = loadDict()
+#v, a = getSentimentScore(['hate', 'sad', 'love'], 1, d)
+#print v, a
