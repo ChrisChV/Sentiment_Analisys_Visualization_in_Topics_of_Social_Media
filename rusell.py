c6825e076bf1bc2fb7cbb8d7462101419af953bd
julitus <julioqq29@gmail.com>
2018-07-06 17:00:52 -0500

    Fix rusell

--- rusell.py
+++ rusell.py
@@ -30,13 +30,14 @@ def loadDict():
 def getSentimentScore(tokens, dictByTopic, dictTokens):
 	num_v, den_v, num_a, den_a = 0, 0, 0, 0
 	for i in range(0, len(tokens)):
-		td = dictByTopic[tokens[i]] if type(dictByTopic) is dict else dictByTopic
-		num_v += ((td * dictTokens[tokens[i]]['v'][0]) / dictTokens[tokens[i]]['v'][1])
-		den_v += (td / dictTokens[tokens[i]]['v'][1])
-		num_a += ((td * dictTokens[tokens[i]]['a'][0]) / dictTokens[tokens[i]]['a'][1])
-		den_a += (td / dictTokens[tokens[i]]['a'][1])
-	return num_v / den_v, num_a / den_a
+		if (tokens[i] in dictTokens):
+			td = dictByTopic[tokens[i]] if type(dictByTopic) is dict else dictByTopic
+			num_v += ((td * dictTokens[tokens[i]]['v'][0]) / dictTokens[tokens[i]]['v'][1])
+			den_v += (td / dictTokens[tokens[i]]['v'][1])
+			num_a += ((td * dictTokens[tokens[i]]['a'][0]) / dictTokens[tokens[i]]['a'][1])
+			den_a += (td / dictTokens[tokens[i]]['a'][1])
+	return num_v / den_v if den_v != 0 else 0, num_a / den_a if den_a != 0 else 0
 
 d = loadDict()
-#v, a = getSentimentScore(['hate', 'sad', 'love'], 1, d)
-#print v, a
+v, a = getSentimentScore(['odio', 'tristeza', 'amor'], 1, d)
+print v, a
